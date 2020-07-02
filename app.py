506c62f22cf27a44362662b6f6d039a96471f7cc
balbino aylagas <balbino_a@hotmail.com>
2020-07-01 23:29:33 -0600

    fix error 001

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", method=["GET", "POST"])
 def hello():
     return render_template("index.html")
 
